d1d3485ca517a4c2c661e82e7fabca59e69edc3f
Alvaro Perez <265096469+mitzdev1999@users.noreply.github.com>
2026-04-10 17:59:18 -0500

    feat(matrices): Operaciones con JavaScript.

--- JavaScript/js_punto_2.js
+++ JavaScript/js_punto_2.js
@@ -7,7 +7,7 @@ console.log("--------------------------")
 console.log()
 let matriz = [
     [1,2,3],
-    [3,5,6],
+    [4,5,6],
     [7,8,9]
 ];
 
@@ -49,6 +49,35 @@ console.log("Recorrido por columnas")
 for (let i=0;i<matriz.length;i++) {
     for (let j=0;j<matriz.length;j++){
         process.stdout.write(matriz[j][i]+ "\t")
+    }
+    console.log("\n")
+}
+console.log()
+console.log("--------------------------")
+console.log("Punto 3, Operaciones")
+console.log("--------------------------")
+console.log()
+console.table(matriz)
+acumulador = 0
+for (let i=0;i<matriz.length;i++) {
+    for (let j=0;j<matriz.length;j++){
+        acumulador = acumulador + matriz[i][j]
+    }
+    
+}
+console.log("la suma de los elementos es igual a:", acumulador)
+console.log()
+console.log("------------------------------")
+console.log("Intercambiar la primera fila con la ultima")
+console.table(matriz)
+console.log("matriz con primera y utlima fila intercambiadas")
+temporal = matriz[0]
+matriz[0] = matriz[2]
+matriz[2] = temporal
+console.table(matriz)
+for (let i=0;i<matriz.length;i++) {
+    for (let j=0;j<matriz.length;j++){
+        process.stdout.write(matriz[i][j]+ "\t")
     }
     console.log("\n")
 }
