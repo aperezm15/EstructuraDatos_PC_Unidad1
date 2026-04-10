7633415b16b2d247274894358cce0cf800d872a2
Alvaro Perez <265096469+mitzdev1999@users.noreply.github.com>
2026-04-10 18:05:45 -0500

    feat(matrices): Declaracion e inicializacion con Dart

---
+++ Dart/dart_punto_2.dart
@@ -0,0 +1,13 @@
+void main() {
+  print("----------------------");
+  print("Punto 1. Declaracion e inicializacion");
+  print("----------------------");
+
+  var matriz = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+  ];
+
+  print(matriz);
+}
