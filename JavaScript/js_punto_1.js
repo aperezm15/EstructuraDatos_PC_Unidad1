1aa8456bae8f474f7df77b635732916d4224ff95
Alvaro Perez <265096469+mitzdev1999@users.noreply.github.com>
2026-04-10 13:46:29 -0500

    feat(arreglos): Recorrido e Impresion en javaScript haciendo for y forEach

--- JavaScript/js_punto_1.js
+++ JavaScript/js_punto_1.js
@@ -4,6 +4,25 @@ sera 10, , utilizamos math.Ramdom, el cual nos dara decimales, pero lo multiplic
 quitar los decimales y que los numeros sean limitados hasta el numero 10, usando el + 1 al final
 para integrar el numero 10, de lo contrario llegaria hasta el 9.
 */
+
+console.log("--------------------------")
+console.log("Punto 1, Declaracion y creacion de un arreglo")
+console.log("--------------------------")
+
 let arreglo = Array.from({ length: 10}, () => Math.floor(Math.random() * 10) + 1);
 /*Utilizamos console.log para imprimir el arreglo y ver los numeros asignados. */
 console.log("Arreglo inicial", arreglo);
+console.log()
+console.log("--------------------------")
+console.log("Punto 2, Recorrido e impresion")
+console.log("--------------------------")
+
+console.log("Recorrido for clasico:")
+for (let i = 0; i < arreglo.length; i++) {
+    console.log(`Indice ${i}: ${arreglo[i]}`)
+}
+console.log()
+console.log("Recorrido forEach")
+arreglo.forEach((valor, index) => {
+    console.log(`Indice ${index}: ${valor}`)
+} )
