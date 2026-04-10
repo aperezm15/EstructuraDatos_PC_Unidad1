054380cc84174552d3b9c8129d5f68b8308dc9d3
Alvaro Perez <265096469+mitzdev1999@users.noreply.github.com>
2026-04-10 18:33:21 -0500

    feat(matrices): Operaciones con DART.

--- Dart/dart_punto_2.dart
+++ Dart/dart_punto_2.dart
@@ -29,4 +29,44 @@ void main() {
     }
     print("");
   }  
+
+  print("----------------------");
+  print("Punto 2. Recorrido");
+  print("----------------------");
+  print("Sumar los elementos de esta matriz");
+  for (int i=0;i<matriz.length;i++) {
+    for (int j=0;j<matriz.length;j++) {
+      stdout.write("${matriz[i][j]}\t");
+    }
+    print("");
+  }
+  print("");
+  int acumulador = 0;
+  for (int i=0;i<matriz.length;i++) {
+    for (int j=0;j<matriz.length;j++) {
+      acumulador = acumulador + matriz[i][j];
+    }
+  }
+
+  stdout.write("La suma de los elementos es: $acumulador");
+  print("-----------------------------------");
+  print("Vamos a intercambiar la primera y ultima fila de esta matriz");
+  for (int i=0;i<matriz.length;i++) {
+    for (int j=0;j<matriz.length;j++) {
+      stdout.write("${matriz[i][j]}\t");
+    }
+    print("");
+  }
+  var temporal = matriz[0];
+  matriz[0] = matriz[2];
+  matriz[2] = temporal;
+  print("");
+  print("Matriz con filas intercambiadas");
+  for (int i=0;i<matriz.length;i++) {
+    for (int j=0;j<matriz.length;j++) {
+      stdout.write("${matriz[i][j]}\t");
+    }
+    print("");
+  }
+
 }
