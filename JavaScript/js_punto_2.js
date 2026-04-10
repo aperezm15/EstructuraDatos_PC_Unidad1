55909c68f7a5527750d398082e4324a5fc4cbab0
Alvaro Perez <265096469+mitzdev1999@users.noreply.github.com>
2026-04-10 17:45:57 -0500

    feat(matrices): Recorrido en JavaScript

--- JavaScript/js_punto_2.js
+++ JavaScript/js_punto_2.js
@@ -1,7 +1,10 @@
 //Matrices
 
 //Declaracion e inicializacion
-
+console.log("--------------------------")
+console.log("Punto 1, Declaracion e incializacion")
+console.log("--------------------------")
+console.log()
 let matriz = [
     [1,2,3],
     [3,5,6],
@@ -17,6 +20,35 @@ console.table(matriz)
 for (let i=0;i<matriz.length;i++) {
     for (let j=0;j<matriz.length;j++){
         process.stdout.write(matriz[i][j]+ "\t")
+    }
+    console.log("\n")
+}
+
+console.log("")
+console.log("--------------------------")
+console.log("Punto 2, Recorrido")
+console.log("--------------------------")
+console.log()
+//imprimir la matriz en forma de tabla
+//como habiamos dicho antes, podemos hacerlo de dieferentes formas
+//con console.table o con for
+console.log("Tabla con console.table")
+console.table(matriz)
+console.log("---------------------------")
+console.log("tabla con for clasico")
+for (let i=0;i<matriz.length;i++) {
+    for (let j=0;j<matriz.length;j++){
+        process.stdout.write(matriz[i][j]+ "\t")
+    }
+    console.log("\n")
+}
+
+//Recorrerla por columnas
+console.log("---------------------------")
+console.log("Recorrido por columnas")
+for (let i=0;i<matriz.length;i++) {
+    for (let j=0;j<matriz.length;j++){
+        process.stdout.write(matriz[j][i]+ "\t")
     }
     console.log("\n")
 }
