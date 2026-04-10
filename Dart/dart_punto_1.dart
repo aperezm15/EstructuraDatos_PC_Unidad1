a356cc8f2fe78e0220119ae1bd1e644c5c1e3aeb
Alvaro Perez <265096469+mitzdev1999@users.noreply.github.com>
2026-04-10 15:54:54 -0500

    feat(arreglos): Modificacion con dart.

--- Dart/dart_punto_1.dart
+++ Dart/dart_punto_1.dart
@@ -27,4 +27,26 @@ void main(){
     print("indice $index: $valor");
     index = index + 1;
   }
+  print("");
+  print("----------------------");
+  print("Punto 3. Modificacion");
+  print("----------------------");
+  print("Cambiar numeros impares por 0");
+  print("Arreglo: $arreglo");
+  for (int i = 0;i < arreglo.length;i++) {
+    if (arreglo[i] % 2 != 0) {
+      arreglo[i] = 0;
+    }
+  } 
+  print("");
+  print("Arreglo con impares igual a 0:");
+  print(arreglo);
+  print("-------------------------");
+  print("Multiplicacion de elemento con indice");
+  List<int> arreglo2 = List.generate(10, (_) => random.nextInt(10) + 1);
+  print("Arreglo: $arreglo2");
+  for(int i=0;i<arreglo2.length;i++) {
+    print("Multiplicacion: indce $i * ${arreglo2[i]} = ${i * arreglo2[i]}");
+  }
+
 }
