b524a77b964122a13c6f3fe09797830ca88790e0
Alvaro Perez <265096469+mitzdev1999@users.noreply.github.com>
2026-04-10 15:33:15 -0500

    feat(arreglos): Recorrido e Impresion en Dart.

--- Dart/dart_punto_1.dart
+++ Dart/dart_punto_1.dart
@@ -5,9 +5,26 @@ void main(){
   print("----------------------");
   print("Punto 1. Declaracion y creacion de un arreglo");
   print("----------------------");
-  
+
   var random = Random();
   List<int> arreglo = List.generate(10, (_) => random.nextInt(10) + 1);
 
   print("Arreglo creado: $arreglo");
+
+  print("");
+  print("----------------------");
+  print("Punto 2. Recorrido e Impresion");
+  print("----------------------");
+  print("Recorrido for clasico");
+  for (int i = 0;i<arreglo.length;i++){
+    print("Indice $i: ${arreglo[i]}");
+  }
+  print("");
+  print("Recorrido For-in, (En este lenguaje se llama asi, pero es el mismo for-each)");
+  print("");
+  int index = 0;
+  for (var valor in arreglo) {
+    print("indice $index: $valor");
+    index = index + 1;
+  }
 }
